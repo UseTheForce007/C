8ed6efce9f6885179390a4a771193d32645da157
UseTheForce007 <buttyusuf@gmail.com>
2023-08-04 19:09:14 +0500

    Added gradient descent function

--- functions.hpp
+++ functions.hpp
@@ -4,17 +4,20 @@
 #include <string>
 #include <vector>
 
-struct DataEntry {
+struct DataEntry
+{
     float temp;     // Label for the sixth column (temperature)
     float salinity; // Label for the seventh column (salinity)
 };
 
-std::vector<DataEntry> readCSV(const std::string& filename);
+std::vector<DataEntry> readCSV(const std::string &filename);
 
-std::pair<double, double> linearRegression(const std::vector<double>& x, const std::vector<double>& y);
+std::pair<double, double> linearRegression(const std::vector<double> &x, const std::vector<double> &y);
 
-double meanSquaredError(const std::vector<double>& predicted, const std::vector<double>& actual);
+double meanSquaredError(const std::vector<double> &predicted, const std::vector<double> &actual);
 
-double rSquared(const std::vector<double>& predicted, const std::vector<double>& actual);
+double rSquared(const std::vector<double> &predicted, const std::vector<double> &actual);
+
+std::pair<double, double> gradientDescent(const Eigen::VectorXd &X, const Eigen::VectorXd &y, double learningRate = 0.01, int numIterations = 1000);
 
 #endif // DATA_ENTRY_H
---
+++ grad_d.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <utility>
+#include <Eigen/Dense>
+
+std::pair<double, double> gradientDescent(const Eigen::VectorXd &X, const Eigen::VectorXd &y, double learningRate = 0.01, int numIterations = 1000)
+{
+    double a = 0.0; // Initial guess for the slope (a)
+    double b = 0.0; // Initial guess for the intercept (b)
+
+    int m = X.size(); // Number of data points
+
+    for (int iter = 0; iter < numIterations; ++iter)
+    {
+        // Calculate the predicted values (y_hat) using the current coefficients a and b
+        Eigen::VectorXd y_hat = X * a + Eigen::VectorXd::Constant(m, b);
+
+        // Calculate the gradients (partial derivatives) of the cost function with respect to a and b
+        double gradient_a = -2.0 * (X.transpose() * (y - y_hat)).sum() / m;
+        double gradient_b = -2.0 * (y - y_hat).sum() / m;
+
+        // Update the coefficients using the learning rate and the gradients
+        a -= learningRate * gradient_a;
+        b -= learningRate * gradient_b;
+    }
+
+    return std::make_pair(a, b);
+}
